350b823b4962bfc0560313b966238b2a358bf887
souravwebsadroit <121152978+souravwebsadroit@users.noreply.github.com>
2023-11-25 17:46:38 +0530

    landing page

--- frontend/src/Theme.js
+++ frontend/src/Theme.js
@@ -3,7 +3,6 @@ import { createTheme } from '@mui/material/styles';
  const theme = createTheme({
   palette: {
     primary: {
-      logoColor: '#1d8ea2',
       main: '#3f50b5',
       dark: '#002884',
       LogoColor:"#27aae1",
@@ -15,6 +14,13 @@ import { createTheme } from '@mui/material/styles';
       ParaColor:"#6d6e71",
       ButtonGredient:"linear-gradient(to bottom, rgba(109,110,113,1), rgba(0,0,0,1))",
       BackGredient:"linear-gradient(to bottom, rgba(43,57,144,.8), rgba(39,170,225,1) 65%)",
+      BackGredient2:"linear-gradient(to bottom, rgba(43,57,144,.8), rgba(39,170,225,.8) 65%)",
+      deepBlue:"#2b3990",
+      BackGredient1:"linear-gradient(to bottom, rgba(43,57,144,100%), rgba(39,170,225,1) 55%)",
+      LightGray1:"#939598",
+      GrayGredient:"-webkit-radial-gradient(#fff 50%, #EFEFEF 90%)",
+      Gray2:"#f8f8f8",
+      BoxShadow: "0 12px 12px rgba(39, 170, 225, 0.3)",
     },
     secondary: {
       light: '#ff7961',
