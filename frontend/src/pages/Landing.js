f56d70427e6dc395323d095b715fcc02466d920c
souravwebsadroit <121152978+souravwebsadroit@users.noreply.github.com>
2023-11-25 12:29:38 +0530

    font lato

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,4 +1,4 @@
-import { Box, Container, } from "@mui/material";
+import { Box, Container, Divider, Typography, } from "@mui/material";
 import React from "react";
 import Header from "../components/Header";
 import theme from "../Theme";
@@ -47,7 +47,7 @@ const Landing = () => {
               display: "flex",
               flexDirection: "column",
               alignItems: "center",
-              justifyContent: "center",
+              justifyContent: "space-between",
               "&::after": {
                 content: "''",
                 position: "absolute",
@@ -73,8 +73,33 @@ const Landing = () => {
                   height: "50px",
                 }}
               />
+             
             </Box>
-            
+            <Box >
+                <Typography sx={{
+                  color: theme.palette.primary.White,
+                  fontWeight: "500",
+                  fontSize: "30px",
+                }} variant="h6" component={"h2"}>Find My Friends</Typography>
+                <Divider sx={{
+                   borderTop: "1px solid",
+                   borderColor:"rgba(255, 255, 255, .20)",
+                   width: "180px",
+                   margin:"auto",
+                   borderBottom:"none"
+                }}/>
+                <Box sx={{
+                  padding:"20px 20px 0",
+                  border:"1px solid rgba(255, 255, 255, .2)",
+                  borderTop:"none",
+                  marginBottom:"30px",
+                  color: theme.palette.primary.White,
+                  textAlign:"center"
+                }}>
+                  <Typography variant="body" component={"p"}>Signup now and meet awesome people around the world</Typography>
+
+                </Box>
+              </Box>
           </Box>
         </Container>
       </Box>
