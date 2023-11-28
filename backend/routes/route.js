57fe736b1812c8599380467f8d5950c87d213c74
Barun Karmakar <121152978+souravwebsadroit@users.noreply.github.com>
2023-11-28 19:04:18 +0530

    sourav28_11_23

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -2,15 +2,41 @@ const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
 const userController = require('../controllers/userController');
+const messageController = require('../controllers/messageController');
+const feedController = require('../controllers/feedController');
 
 //user API
 router.post('/register', userController.register);
 router.post('/login', userController.login);
+router.get('/profile', [authMiddleware.isAuth], userController.profile);
+router.put('/update-profile', [authMiddleware.isAuth], userController.updateProfile);
 //send user friend request
 router.post('/send-friend-request', [authMiddleware.isAuth], userController.sendFriendRequest);
 //friend request list
 router.get('/friend-request-list', [authMiddleware.isAuth], userController.friendRequestList);
-//frind request status update
+//friend request status update
 router.put('/friend-request-status-update', [authMiddleware.isAuth], userController.friendRequestStatusUpdate);
+//friend list
+router.get('/friend-list', [authMiddleware.isAuth], userController.friendList);
+//follow user
+router.post('/follow-user', [authMiddleware.isAuth], userController.followUser);
+//follow user list
+router.get('/follow-user-list', [authMiddleware.isAuth], userController.followUserList);
+//send message
+router.post('/send-message', [authMiddleware.isAuth], messageController.sendMessage);
+//message list
+router.get('/message-list', [authMiddleware.isAuth], messageController.messageList);
+//feed API
+router.post('/create-feed', [authMiddleware.isAuth], feedController.createFeed);
+//feed list
+router.get('/feed-list', [authMiddleware.isAuth], feedController.feedList);
+//reacting on feed
+router.post('/react-on-feed', [authMiddleware.isAuth], feedController.reactOnFeed);
+//comment on feed
+router.post('/comment-on-feed', [authMiddleware.isAuth], feedController.commentOnFeed);
+//reply on comment
+router.post('/reply-on-comment', [authMiddleware.isAuth], feedController.replyOnComment);
+//tag friend on feed
+router.post('/tag-friend-on-feed', [authMiddleware.isAuth], feedController.tagFriendOnFeed);
 
 module.exports = router;
