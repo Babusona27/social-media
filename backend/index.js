266d2891cf8314de4de045918c7821e62be51112
Barun Karmakar <121152978+souravwebsadroit@users.noreply.github.com>
2023-12-01 13:15:47 +0530

    cors issue

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const http = require('http'); // import http
 const socketIO = require('socket.io'); // import socket.io
 const app = express();
-const server = http.createServer(app); // create server
-const io = socketIO(server); // create socket using server
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
@@ -18,35 +16,45 @@ app.use(cors());
 
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
-    console.log('Connected to database!');
+  console.log('Connected to database!');
 }).catch(() => {
-    console.log('Connection failed!');
+  console.log('Connection failed!');
 });
 app.use("/social/api/v1", route);
 
 
+
+
+const port = process.env.PORT || 4000;
+const server = app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
+const io = socketIO(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
+  }
+});
+
 // Listen for socket connections
 io.on('connection', (socket) => {
-    console.log('A user connected');
-  
-    // Listen for private messages
-    socket.on('private_message', (data) => {
-        console.log("data==>",data);
-      // Emit the private message to the target user
-      io.to(data.targetUserId).emit('private_message', {
-        senderUserId: socket.id,
-        message: data.message,
-      });
-    });
-  
-    // Listen for disconnections
-    socket.on('disconnect', () => {
-      console.log('User disconnected');
+  console.log('A user connected');
+
+  // Listen for private messages
+  socket.on('private_message', (data) => {
+    console.log("data==>", data);
+    // Emit the private message to the target user
+    io.to(data.targetUserId).emit('private_message', {
+      senderUserId: socket.id,
+      message: data.message,
     });
   });
 
-const port = process.env.PORT || 4000;
-server.listen(port, () => {
-    console.log(`server is running on port ${port}`);
+  // Listen for disconnections
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
 });
 
